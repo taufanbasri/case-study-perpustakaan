b1aa36e0c3db5d8879e6de84dd1a3b1455bf2f5c
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-04-13 10:17:46 +0700

    Using Seeder - make seeder with php artisan make:seeder UsersSeeder

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Role;
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Membuat role Admin
+        $roleAdmin = Role::create([
+            'name' => 'admin',
+            'display_name' => 'Admin'
+        ]);
+
+        // Membuat role Member
+        $roleMember = Role::create([
+            'name' => 'member',
+            'display_name' => 'Member'
+        ]);
+
+        // Contoh user dengan role Admin
+        $adminUser = User::create([
+            'name' => 'Admin Tamvan',
+            'email' => 'admin@mail.com',
+            'password' => bcrypt('123456'),
+        ]);
+
+        $adminUser->attachRole($roleAdmin);
+
+        // Contoh user dengan role Member
+        $memberUser = User::create([
+            'name' => 'Member Soleha',
+            'email' => 'member@mail.com',
+            'password' => bcrypt('123456'),
+        ]);
+
+        $memberUser->attachRole($roleMember);
+    }
+}
