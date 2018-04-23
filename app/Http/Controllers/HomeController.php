2a6c6cc6a2e08da065cd73ea16038f17759f324e
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-04-23 10:11:06 +0700

    Pembedaan dashboard member & admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Laratrust\LaratrustFacade;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,24 @@ class HomeController extends Controller
      */
     public function index()
     {
+        if (LaratrustFacade::hasRole('admin')) {
+            return $this->adminDashboard();
+        }
+
+        if (LaratrustFacade::hasRole('member')) {
+            return $this->memberDashboard();
+        }
+
         return view('home');
+    }
+
+    public function adminDashboard()
+    {
+        return view('dashboard.admin');
+    }
+
+    public function memberDashboard()
+    {
+        return view('dashboard.member');
     }
 }
