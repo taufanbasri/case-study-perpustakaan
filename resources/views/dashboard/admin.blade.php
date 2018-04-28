2529c8810ac96ddf18a0826fe887cf967a6b4bcf
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-04-28 17:01:47 +0700

    Using Chartjs

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -8,9 +8,47 @@
         <div class="card-header">Dashboard</div>
         <div class="card-body">
           Selamat datang Admin!!
+          <hr>
+          <h4>Statistik Penulis</h4>
+          <canvas id="chartPenulis" width="400" height="150"></canvas>
         </div>
       </div>
     </div>
   </div>
 </div>
 @endsection
+
+@push('scripts')
+  <script src="{{ asset('js/Chart.min.js') }}"></script>
+
+  <script>
+    var dataChart = {
+      labels: {!! json_encode($authors) !!},
+      datasets: [{
+        label: 'Jumlah Buku',
+        data: {!! json_encode($books) !!},
+        backgroundColor: "rgba(151,187,205,0.5)",
+        borderColor: "rgba(151,187,205,0.8)",
+      }]
+    };
+
+    var options = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true,
+            stepSize: 1
+          }
+        }]
+      }
+    };
+
+    var ctx = document.getElementById("chartPenulis").getContext("2d");
+
+    var authorChart = new Chart(ctx, {
+      type: 'polarArea',
+      data: dataChart,
+      option: options
+    });
+  </script>
+@endpush
